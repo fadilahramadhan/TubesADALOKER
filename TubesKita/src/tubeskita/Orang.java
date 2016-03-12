03ac26e50bd7a6a16d9a4eb4d5eb6111b7ea739e
eri <esadewo20gmail.com>
2016-03-12 12:06:46 +0700

    Orang

--- TubesKita/src/tubeskita/Orang.java
+++ TubesKita/src/tubeskita/Orang.java
@@ -7,8 +7,51 @@ package tubeskita;
 
 /**
  *
- * @author Acer
+ * @author Eri Sadewo - 1301140022 - if 38 02
  */
 public abstract class Orang {
-    
+	private String nama; 
+	private String alamat; 
+	private String jenisKelamin;
+	private String usia;
+	private int nope;
+	
+	public void setnama(String nama){
+		this.nama = nama;
+	}
+	
+	public String getnama(){
+		return nama;
+	}
+	
+	public void setalamat(String alamat){
+		this.alamat = alamat;
+	}
+	
+	public String getalamat(){
+		return alamat;
+	}
+	
+	public void setjenisKelamin(String jenisKelamin){
+		this.jenisKelamin = jenisKelamin;
+	}
+	
+	public String getjenisKelamin(){
+		return jenisKelamin;
+	}
+	
+	public void setusia(String usia){
+		this.usia = usia;
+	}
+	public String getusia(){
+		return usia;
+	}
+	public void setnope(int nope){
+		this.nope = nope;
+	}
+	public int getnope(){
+		return nope;
+	}
 }
+    
+
