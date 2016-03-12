246a09e2449456f3f6e5d2d04f9d9f284321c9c8
fadilahramadhan <fadilah.gudil@gmail.com>
2016-03-12 17:38:47 +0700

    orang,pelamar,perusahaan,berkaslamaran,lowongan syudahh

--- TubesKita/src/tubeskita/Orang.java
+++ TubesKita/src/tubeskita/Orang.java
@@ -10,5 +10,79 @@ package tubeskita;
  * @author Acer
  */
 public abstract class Orang {
-    
+    private String nama;
+    private String alamat;
+    private String jenisKelamin;
+    private int usia;
+    private int noPe;
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the jenisKelamin
+     */
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    /**
+     * @param jenisKelamin the jenisKelamin to set
+     */
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    /**
+     * @return the usia
+     */
+    public int getUsia() {
+        return usia;
+    }
+
+    /**
+     * @param usia the usia to set
+     */
+    public void setUsia(int usia) {
+        this.usia = usia;
+    }
+
+    /**
+     * @return the noPe
+     */
+    public int getNoPe() {
+        return noPe;
+    }
+
+    /**
+     * @param noPe the noPe to set
+     */
+    public void setNoPe(int noPe) {
+        this.noPe = noPe;
+    }
 }
