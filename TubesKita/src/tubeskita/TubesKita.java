620c31a430db69365783870b2d9cd28cf78a6a63
eri <esadewo20gmail.com>
2016-03-12 11:49:00 +0700

    Orang

--- TubesKita/src/tubeskita/TubesKita.java
+++ TubesKita/src/tubeskita/TubesKita.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,9 +8,53 @@ package tubeskita;
 
 /**
  *
- * @author Acer
+ * @author Eri Sadewo/If 38-02/1301140022
  */
 public class TubesKita {
+    abstract class Orang{
+	private String nama; 
+	private String alamat; 
+	private String jenisKelamin;
+	private String usia;
+	private int nope;
+        
+	public void setnama(String nama){
+		this.nama = nama;
+	}
+	
+	public String getnama(){
+		return nama;
+	}
+	
+	public void setalamat(String alamat){
+		this.alamat = alamat;
+	}
+	
+	public String getalamat(){
+		return alamat;
+	}
+	
+	public void setjenisKelamin(String jeniskelamin){
+		this.jenisKelamin = jeniskelamin;
+	}
+	
+	public String getjenisKelamin(){
+		return jenisKelamin;
+	}
+	
+	public void setusia(String usia){
+		this.usia = usia;
+	}
+	public String getusia(){
+		return usia;
+	}
+	public void setnope(int nope){
+		this.nope = nope;
+	}
+	public int getnope(){
+		return nope;
+	}
+    }
 
     /**
      * @param args the command line arguments
